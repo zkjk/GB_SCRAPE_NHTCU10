b35275f468b34e6a367ad3efed94282b4e5c609b
zkjk <48961529+zkjk@users.noreply.github.com>
2019-06-08 18:08:37 +0200

    Now able to scrape through anything you ask
    # enter this link in the command line: scrapy crawl CYR_FINAL -a ip='name_of_search' -o "name_of_file".json
    # as this scrapes every result with the saerch query you asked for, cancel with cntrl + c (ONCE!)

--- Scraper/CompanyUK/CompanyUK/spiders/CYR_FINAL.py
+++ Scraper/CompanyUK/CompanyUK/spiders/CYR_FINAL.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Author Zakaria El-Bouchahati
-# import scrapy and regex AND LinkExtractor
-# This program scrapes through the comapnies in the uk website to run it
-# enter this link in the command line: scrapy crawl CYR_FINAL -o "name_of_file".json
-# as this scrapes every result with "a" cancel with cntrl + c (ONCE!)
 
+# This program scrapes through the comapnies in the uk website to run it
+# enter this link in the command line: scrapy crawl CYR_FINAL -a ip='name_of_search' -o "name_of_file".json
+# as this scrapes every result with the saerch query you asked for, cancel with cntrl + c (ONCE!)
+
+# import scrapy and regex AND LinkExtractor AND uuid
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 import datetime
@@ -15,7 +16,17 @@ import uuid
 class QuotesSpider(scrapy.Spider):
 
   name = 'CYR_FINAL'
-  start_urls = ['https://www.companiesintheuk.co.uk/siccode/0']
+  start_urls = ['https://www.companiesintheuk.co.uk/Company/Find?q=']
+
+  def start_requests(self):
+        # self points to the spider instance
+        # that was initialized by the scrapy framework when starting a crawl
+        # spider instances are "augmented" with crawl arguments
+        # available as instance attributes,
+        # self.ip has the (string) value passed on the command line
+        # with `-a ip=somevalue`
+    for url in self.start_urls:
+      yield scrapy.Request(url + self.ip, dont_filter=True)
 
   def parse(self, response):
     # This part extract the search result item page and makes parse_details method go through it
@@ -42,26 +53,13 @@ class QuotesSpider(scrapy.Spider):
 
     # Looping throught the searchResult block and yielding it
     for i in response.css('#content2'):
-        yield {
-            'company_name': i.css('[itemprop="name"]::text').get(),
-            'company_registration_no': i.css('#content2 > div:nth-child(6) > div:nth-child(2)::text').extract_first(),
-            'address': i.css('[itemprop="streetAddress"]::text').extract_first(),
-            'location': i.css("[itemprop='addressLocality']::text").extract_first(),
-            'postal_code': i.css("[itemprop='postalCode']::text").extract_first(),
-            'land_code': i.css("test").extract_first(default="GB"),
-            'date_time': datetime.datetime.now(),
-            'uid': str(uuid.uuid4())
-        }
-
-
-##############################################################################################
-# test code
-# for i in response.css('div.col-md-6'):
-#    if not i.css('#content2 > strong:nth-child(2) > strong:nth-child(1)'):
-#        continue
-#    yield {
-#        'company_name': i.css('#content2 > strong:nth-child(2) > strong:nth-child(1) > div:nth-child(1)::text').get(),
-#        'address': i.css("#content2 > strong:nth-child(2) > address:nth-child(2) > div:nth-child(1) > span:nth-child(1)::text").extract_first(),
-#        'location': i.css("#content2 > strong:nth-child(2) > address:nth-child(2) > div:nth-child(1) > span:nth-child(3)::text").extract_first(),
-#        'postal_code': i.css("#content2 > strong:nth-child(2) > address:nth-child(2) > div:nth-child(1) > a:nth-child(9) > span:nth-child(1)::text").extract_first(),
-#    }
+      yield {
+          'company_name': i.css('[itemprop="name"]::text').get(),
+          'company_registration_no': i.css('#content2 > div:nth-child(6) > div:nth-child(2)::text').extract_first(),
+          'address': i.css('[itemprop="streetAddress"]::text').extract_first(),
+          'location': i.css("[itemprop='addressLocality']::text").extract_first(),
+          'postal_code': i.css("[itemprop='postalCode']::text").extract_first(),
+          'land_code': i.css("test").extract_first(default="GB"),
+          'date_time': datetime.datetime.now(),
+          'uid': str(uuid.uuid4())
+      }
