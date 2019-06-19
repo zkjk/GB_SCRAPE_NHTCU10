0e25df7a55e78009254ef4f97e427a8f36343fd4
hoyeunglam <31922701+hoyeunglam@users.noreply.github.com>
2019-06-19 11:06:50 -0500

    Update SceyeDb.py

--- SceyeDb.py
+++ SceyeDb.py
@@ -20,16 +20,6 @@ def main(arg1):
 
         # parse json data to SQL insert
         for i, item in enumerate(json_obj):
-            print("date_time:", item["date_time"])
-            print("uid:", item["uid"])
-            print("land_code:", item["land_code"])
-            print("company_registration_no", item["company_registration_no"])
-            print("postal_code:", item["postal_code"])
-            print("company_name:", item["company_name"])
-            print("address:", item["address"])
-            print("location:", item["location"])
-            print("url:", item["url"])
-
             cursor.execute("INSERT INTO Company (UUIDCompany, Name, LandCode, ChamberOfCommerce, ScrapeDate, Source) VALUES (%s,%s,%s,%s,%s,%s)", (item["uid"], item["company_name"], item["land_code"], item["company_registration_no"], item["date_time"], item["url"]))
             
     con.commit()
